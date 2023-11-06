42d5abc77bd537f53aae7580374283e8e7b9ff6b
EgeKorkmaz <zamkorkege@gmail.com>
2023-11-06 16:01:58 +0300

    Blink animation class fixed.

--- app/src/main/java/testClasses/CustomAnimations.kt
+++ app/src/main/java/testClasses/CustomAnimations.kt
@@ -6,25 +6,13 @@ import com.example.sukruegekorkmaz_hw1.R
 
 class CustomAnimations() {
     companion object{
+        var blincDuration = 750
         fun blink(tvTemp : TextView){
-            val blinkAnimation = AlphaAnimation(1f, 0f)
-            var isBlinking = false
+            val blinkAnimation = AlphaAnimation(0f, 1f)
 
-            blinkAnimation.duration = 500 // Set the duration of the animation in milliseconds
-            blinkAnimation.repeatCount = Animation.INFINITE // Infinite blinking
-
-            // Set an AnimationListener to toggle the visibility of the TextView
-            blinkAnimation.setAnimationListener(object : Animation.AnimationListener {
-                override fun onAnimationStart(animation: Animation) {}
-
-                override fun onAnimationEnd(animation: Animation) {
-                    // Toggle visibility when the animation ends
-                    tvTemp.visibility = if (isBlinking) View.VISIBLE else View.INVISIBLE
-                    isBlinking = !isBlinking
-                }
-
-                override fun onAnimationRepeat(animation: Animation) {}
-            })
+            blinkAnimation.duration = blincDuration.toLong() // Set the duration of the animation in milliseconds
+            blinkAnimation.repeatCount = Animation.INFINITE
+            blinkAnimation.repeatMode = Animation.REVERSE
 
             tvTemp.startAnimation(blinkAnimation)
         }
