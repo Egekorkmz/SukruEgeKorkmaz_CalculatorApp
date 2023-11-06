3d7c7cb325fc8c9fe75fb5b764d914329db20f2b
EgeKorkmaz <zamkorkege@gmail.com>
2023-11-06 13:20:50 +0300

    Initial commit

---
+++ app/src/main/java/com/example/sukruegekorkmaz_hw1/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.sukruegekorkmaz_hw1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.WindowManager
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //remove status and action bar
+        this.window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
+        supportActionBar?.hide()
+
+    }
+}
